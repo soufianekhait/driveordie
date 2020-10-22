eeabe124efc82615d951c0a1fa6f6ea225dfbe35
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2020-10-22 09:41:56 +0200

    cmake debug configuration

---
+++ cmake-build-debug/CMakeFiles/driveordie.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/driveordie.dir/main.c.obj"
+  "driveordie.exe"
+  "driveordie.exe.manifest"
+  "driveordie.pdb"
+  "libdriveordie.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/driveordie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
