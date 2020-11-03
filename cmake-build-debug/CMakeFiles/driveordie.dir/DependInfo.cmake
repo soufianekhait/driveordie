8cb8e767ffa871a99a6bc138746d09ef7217e561
Alexandre Lepage <alexandre.lepage999@gmail.com>
2020-11-03 09:18:06 +0100

    Esquisse 1

--- cmake-build-debug/CMakeFiles/driveordie.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/driveordie.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/soufi/CLionProjects/driveordie/main.c" "C:/Users/soufi/CLionProjects/driveordie/cmake-build-debug/CMakeFiles/driveordie.dir/main.c.obj"
+  "C:/Users/Alex/Desktop/ENAC/S7/Langage C/driveordie/main.c" "C:/Users/Alex/Desktop/ENAC/S7/Langage C/driveordie/cmake-build-debug/CMakeFiles/driveordie.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- main.c
+++ main.c
@@ -1,46 +1,119 @@
-//Using SDL and standard IO
-#include <SDL.h>
-#include <stdio.h>
+////Using SDL and standard IO
+//#include <SDL.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+////Screen dimension constants
+//const int SCREEN_WIDTH = 840;
+//const int SCREEN_HEIGHT = 680;
+//
+//
+//int main( int argc, char* args[] )
+//{
+//    SDL_Window * ecran = NULL;
+//    SDL_Renderer* rendu = NULL;
+////    SDL_Surface* screensurface = NULL;
+////    SDL_Surface *rectangle = NULL;
+//    SDL_Rect position;
+//    const char *title = "DOD";
+//
+//    SDL_Init(SDL_INIT_VIDEO);
+//
+//    ecran = SDL_CreateWindow(title, 440, 200, 800, 550, 0);
+//    // Allocation de la surface
+////    rectangle = SDL_CreateRGBSurface(SDL_SWSURFACE, 220, 180, 32, 0, 0, 0, 0);
+//    SDL_SetWindowTitle(ecran, "DriveOrDie");
+//
+//    // Create Renderer
+//    SDL_CreateRenderer(ecran, -1 , SDL_RENDERER_ACCELERATED);
+//    //Render red filled quad
+//    SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+//    SDL_SetRenderDrawColor( rendu, 0xFF, 0x00, 0x00, 0xFF );
+//    SDL_RenderFillRect( rendu, &fillRect );
+//
+//    SDL_RenderPresent(rendu); // Mise à jour de l'écran
+//
+//    SDL_Delay(2000);
+//
+//    SDL_RenderClear(rendu); // Libération de la surface
+//    SDL_Quit();
+//
+//    return EXIT_SUCCESS;
+//}
 
-//Screen dimension constants
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 const int SCREEN_WIDTH = 840;
 const int SCREEN_HEIGHT = 680;
 
-
-int main( int argc, char* args[] )
+int main(int argc, char *argv[])
 {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    else{
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial",
-                                   SDL_WINDOWPOS_UNDEFINED,
-                                   SDL_WINDOWPOS_UNDEFINED,
-                                   SCREEN_WIDTH,
-                                   SCREEN_HEIGHT,
-                                   SDL_WINDOW_SHOWN );
-        if( window == NULL )
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        else{
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xBB, 0xFF, 0xFF ) );
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-            //Wait two seconds
-            SDL_Delay( 2000 );
-        }
-    }
-    //Destroy window
-    SDL_DestroyWindow( window );
-    //Quit SDL subsystems
-    SDL_Quit();
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    int statut = EXIT_FAILURE;
+    SDL_Color orange = {255, 127, 40, 255};
 
-    return 0 ;
+    /* Initialisation, création de la fenêtre et du renderer. */
+    if(0 != SDL_Init(SDL_INIT_VIDEO))
+    {
+        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
+        goto Quit;
+    }
+    window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_SetWindowTitle(window, "DriveOrDie");
+    if(NULL == window)
+    {
+        fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
+        goto Quit;
+    }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(NULL == renderer)
+    {
+        fprintf(stderr, "Erreur SDL_CreateRenderer : %s", SDL_GetError());
+        goto Quit;
+    }
+
+//    if (SDL_SetWindowFullscreen(window , SDL_TRUE) < 0)
+//    {
+//        printf("Erreur lors du passage en mode plein ecran : %s", SDL_GetError());
+//        return EXIT_FAILURE;
+//    }
+
+    /* C’est à partir de maintenant que ça se passe. */
+    if(0 != SDL_SetRenderDrawColor(renderer, orange.r, orange.g, orange.b, orange.a))
+    {
+        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
+        goto Quit;
+    }
+
+    if(0 != SDL_RenderClear(renderer))
+    {
+        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
+        goto Quit;
+    }
+
+    SDL_Rect route = { SCREEN_WIDTH / 4, 0, SCREEN_WIDTH /2, SCREEN_HEIGHT };
+    SDL_SetRenderDrawColor( renderer, 0x00, 0x00, 0x00, 0xFF );
+    SDL_RenderFillRect( renderer, &route );
+    SDL_Rect bandegauche = {(SCREEN_WIDTH/4)+5, 0, 15, SCREEN_HEIGHT};
+    SDL_Rect bandedroite = {SCREEN_WIDTH-((SCREEN_WIDTH/4)+20), 0, 15, SCREEN_HEIGHT};
+    SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+    SDL_RenderFillRect(renderer, &bandedroite);
+    SDL_RenderFillRect(renderer, &bandegauche);
+    SDL_Delay(500);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(2000);
+
+    statut = EXIT_SUCCESS;
+
+    Quit:
+    if(NULL != renderer)
+        SDL_DestroyRenderer(renderer);
+    if(NULL != window)
+        SDL_DestroyWindow(window);
+    SDL_Quit();
+    return statut;
 }
