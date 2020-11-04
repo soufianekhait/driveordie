7475ca176b07e8db191e7a515d286393b7a85bae
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2020-11-04 16:28:16 +0100

    Update main.c functions

--- main.c
+++ main.c
@@ -6,61 +6,53 @@
 const int SCREEN_WIDTH = 840;
 const int SCREEN_HEIGHT = 680;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
     int statut = EXIT_FAILURE;
     SDL_Color orange = {255, 127, 40, 255};
 
     /* Initialisation, création de la fenêtre et du renderer. */
-    if(0 != SDL_Init(SDL_INIT_VIDEO))
-    {
-        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
-        goto Quit;
-    }
-    window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              SCREEN_WIDTH, SCREEN_HEIGHT*1.2, SDL_WINDOW_SHOWN);
-    SDL_SetWindowTitle(window, "DriveOrDie");
-    if(NULL == window)
-    {
-        fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
-        goto Quit;
-    }
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if(NULL == renderer)
-    {
-        fprintf(stderr, "Erreur SDL_CreateRenderer : %s", SDL_GetError());
-        goto Quit;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        fprintf(stderr, "SDL_Init Error: %s", SDL_GetError());
+        return EXIT_FAILURE;
     }
 
-//    if (SDL_SetWindowFullscreen(window , SDL_TRUE) < 0)
-//    {
-//        printf("Erreur lors du passage en mode plein ecran : %s", SDL_GetError());
-//        return EXIT_FAILURE;
-//    }
+    window = SDL_CreateWindow("Drive or Die",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              SCREEN_WIDTH,
+                              SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+    SDL_SetWindowTitle(window, "DriveOrDie");
+
+    if(window == NULL){
+        fprintf(stderr, "SDL_CreateWindow Error: %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(renderer == NULL){
+        fprintf(stderr, "SDL_CreateRenderer Error: %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
 
     /* C’est à partir de maintenant que ça se passe. */
-    if(0 != SDL_SetRenderDrawColor(renderer, orange.r, orange.g, orange.b, orange.a))
-    {
-        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
-        goto Quit;
+    if(SDL_SetRenderDrawColor(renderer, orange.r, orange.g, orange.b, orange.a) < 0){
+        fprintf(stderr, "SDL_SetRenderDrawColor Error: %s", SDL_GetError());
+        return EXIT_FAILURE;
     }
 
-    if(0 != SDL_RenderClear(renderer))
-    {
-        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
-        goto Quit;
+    if(SDL_RenderClear(renderer) != 0){
+        fprintf(stderr, "SDL_RenderClear Error: %s", SDL_GetError());
+        return EXIT_FAILURE;
     }
 
     road(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-    statut = EXIT_SUCCESS;
 
-    Quit:
-    if(NULL != renderer)
-        SDL_DestroyRenderer(renderer);
-    if(NULL != window)
-        SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
-    return statut;
+
+    return 0;
 }
