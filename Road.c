dc6ad1869065451713f218e657fd24ce2bdd5f32
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2020-11-08 01:20:17 +0100

    Update solid lines & dashed lines code : setting positions (x,y)

--- Road.c
+++ Road.c
@@ -2,75 +2,95 @@
 #include "Road.h"
 
 
-void init_road(SDL_Renderer* renderer, int SCREEN_WIDTH, int SCREEN_HEIGHT){
+int init_road(SDL_Renderer* renderer){
     // define a rectangle
-    SDL_Rect route = { SCREEN_WIDTH / 4, 0, SCREEN_WIDTH/2, SCREEN_HEIGHT*2};
+    SDL_Rect route = { SCREEN_WIDTH/4, 0, SCREEN_WIDTH/2, SCREEN_HEIGHT*2 };
     // fill it with brown color
     SDL_SetRenderDrawColor(renderer, 201, 173, 167, 0);
-
+    // draw rectangle
     SDL_RenderFillRect(renderer, &route);
 
-    SDL_Rect bandeslaterales[8]; // Création des bandes latérales colonne par colonne
-    bandeslaterales[0].x=bandeslaterales[1].x=bandeslaterales[2].x=bandeslaterales[3].x=(SCREEN_WIDTH/4)+5;
-    bandeslaterales[4].x=bandeslaterales[5].x=bandeslaterales[6].x=bandeslaterales[7].x=SCREEN_WIDTH-((SCREEN_WIDTH/4)+20);
-    bandeslaterales[0].y=bandeslaterales[4].y=5;
-    bandeslaterales[1].y=bandeslaterales[5].y=(SCREEN_HEIGHT/2)+5;
-    bandeslaterales[2].y=bandeslaterales[6].y=SCREEN_HEIGHT+5;
-    bandeslaterales[3].y=bandeslaterales[7].y=SCREEN_HEIGHT+(SCREEN_HEIGHT/2)+5;
-
-    for(int i = 0; i!=8;i++)
-    {
-        bandeslaterales[i].w = 12;
-        bandeslaterales[i].h = 320;
+    SDL_Rect solidLines[7];
+    // Assign dimensions to solid lines
+    for(int i=0; i<7; i++){
+        solidLines[i].w = 15;
+        solidLines[i].h = 320;
     }
 
-    SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
-    SDL_RenderFillRects(renderer, bandeslaterales, 8);
-
-    SDL_Rect pointilles[30]; // Création des bandes du milieu de la route colonne par colonne
-    for (int j=0; j!=30;j++){
-        if (j<20){
-            if (j<10)
-                pointilles[j].x= (SCREEN_WIDTH/3)+30;
-            else
-                pointilles[j].x= (SCREEN_WIDTH/2)-5;
+    // Set solid lines positions
+    int cpt = 4;
+    int y = 5;
+    for(int j=0; j<7; j++){
+        if (j<4) {
+            solidLines[j].x = solidLines[j+1].x = (SCREEN_WIDTH/4) + 5;
+            solidLines[j].y = solidLines[cpt].y = y;
+            y += SCREEN_HEIGHT/2;
+            cpt ++;
+        } else {
+            solidLines[j].x = solidLines[j+1].x = SCREEN_WIDTH - ((SCREEN_WIDTH/4) + solidLines[j].w + 5);
         }
-        else pointilles[j].x= SCREEN_WIDTH-((SCREEN_WIDTH/3)+30);
     }
 
-    for (int k=0;k<30;k++){
-        if (k%10==0)pointilles[k].y= 25;
-        if (k%10==1)pointilles[k].y= 159;
-        if (k%10==2)pointilles[k].y= 293;
-        if (k%10==3)pointilles[k].y= 427;
-        if (k%10==4)pointilles[k].y= 561;
-        if (k%10==5)pointilles[k].y= 696;
-        if (k%10==6)pointilles[k].y= 831;
-        if (k%10==7)pointilles[k].y= 966;
-        if (k%10==8)pointilles[k].y= 1101;
-        if (k%10==9)pointilles[k].y= 1236;
-
+    // fill solid lines with white color
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    // draw rectangles
+    if(SDL_RenderFillRects(renderer, solidLines, 7) < 0){
+        printf("SDL_RenderFillRects for solid lines: %s", SDL_GetError());
+        return EXIT_FAILURE;
     }
 
-    for(int i = 0; i!=30;i++)
-    {
-        pointilles[i].w = 5;
-        pointilles[i].h = 60;
+    // set dashed lines dimensions
+    SDL_Rect dashedLines[18];
+    for(int i=0; i<18; i++){
+        dashedLines[i].w = 5;
+        dashedLines[i].h = 60;
     }
 
-    SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
-    SDL_RenderFillRects(renderer, pointilles, 30);
+    // set dashed lines positions
+    int pos = 25;
+    for (int j=0; j<18; j++){
+        if (j<6)
+            dashedLines[j].x = (SCREEN_WIDTH/3) + 30;
+        else if (j<12){
+            dashedLines[j].x = (SCREEN_WIDTH/2) - dashedLines[j].w;
+            if (j==6) pos = 25;
+        } else{
+            dashedLines[j].x = SCREEN_WIDTH - ((SCREEN_WIDTH/3) + 30);
+            if (j==12) pos = 25;
+        }
+        dashedLines[j].y = pos;
+        pos += SCREEN_HEIGHT/5;
+    }
 
+    // set dashed lines color
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    // draw dashed lines
+    if(SDL_RenderFillRects(renderer, dashedLines, 18) < 0){
+        printf("SDL_RenderFillRects for dashed lines: %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    /*SDL_Surface* image = IMG_Load("resources/img/car.png");
+    if(!image){
+        printf("Error while loading image: %s",SDL_GetError());
+        return -1;
+    }
+    // free surface memory space
+    SDL_FreeSurface(image);*/
+
+    //SDL_RenderSetViewport(renderer, &route);
     // Show renderer's content
     SDL_RenderPresent(renderer);
 
+    // while the user doesn't close the window
     int quit = 0;
     SDL_Event e;
-    while (!quit) {
+    while (!quit){
         while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
+            if (e.type == SDL_QUIT)
                 quit = 1;
-            }
         }
     }
+
+    return 0;
 }
