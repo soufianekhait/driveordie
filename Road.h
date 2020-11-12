0b737db3a445ea05c44aa7e22880ac34801e4e88
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2020-11-12 20:37:14 +0100

    Include SDL_image library and add an SDL_Texture to the init_road() signature

--- Road.h
+++ Road.h
@@ -2,12 +2,13 @@
 #ifndef DRIVEORDIE_ROAD_H
 #define DRIVEORDIE_ROAD_H
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #define SCREEN_WIDTH 840
 #define SCREEN_HEIGHT 680
 
-int init_road(SDL_Renderer*);
+int init_road(SDL_Renderer*, SDL_Texture*);
 
 #endif //DRIVEORDIE_ROAD_H
