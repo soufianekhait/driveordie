cb0c47a8f1dcf536b5b026becf266d61a2c92881
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2020-11-12 20:38:03 +0100

    Create a new background texture

--- main.c
+++ main.c
@@ -41,9 +41,10 @@ const int DOT_HEIGHT = 20;
 // DOT Velocity
 const int DOT_VEL = 10;
 
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
 int main(int argc, char *argv[]){
-    SDL_Window *window = NULL;
-    SDL_Renderer *renderer = NULL;
     SDL_DisplayMode dm;
     SDL_Color orange = {255, 127, 40, 255};
     SDL_Color green = { 87, 167, 115, 255 };
@@ -53,6 +54,13 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "SDL_Init Error: %s", SDL_GetError());
         return EXIT_FAILURE;
     }
+    // initialize SDL Image
+    /*if(!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)){
+        fprintf(stderr, "SDL_Init Error: %s", IMG_GetError());
+        return EXIT_FAILURE;
+    } else{
+        printf("Hey");
+    }*/
     // get actual desktop height
     if (SDL_GetDesktopDisplayMode(0, &dm) != 0) {
         SDL_Log("SDL_GetDesktopDisplayMode failed: %s", SDL_GetError());
@@ -63,7 +71,8 @@ int main(int argc, char *argv[]){
                                   SDL_WINDOWPOS_CENTERED,
                                   SDL_WINDOWPOS_CENTERED,
                                   SCREEN_WIDTH,
-                                  dm.h-80,
+                                  //dm.h-80,
+                                  SCREEN_HEIGHT,
                                   SDL_WINDOW_SHOWN);
         // check if window exists
         if(window == NULL){
@@ -80,6 +89,16 @@ int main(int argc, char *argv[]){
         return EXIT_FAILURE;
     }
 
+    SDL_Texture *background = SDL_CreateTexture(renderer,
+                                                SDL_PIXELFORMAT_RGBA8888,
+                                                SDL_TEXTUREACCESS_TARGET,
+                                                SCREEN_WIDTH, SCREEN_HEIGHT*2);
+
+    if(background == NULL){
+        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    //
     // define a color for renderer
     if(SDL_SetRenderDrawColor(renderer, green.r, green.g, green.b, green.a) < 0){
         fprintf(stderr, "SDL_SetRenderDrawColor Error: %s", SDL_GetError());
@@ -90,15 +109,17 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "SDL_RenderClear Error: %s", SDL_GetError());
         return EXIT_FAILURE;
     }
-
-    printf("Your final score is : %03d\n",function_score());
-
+    //printf("Your final score is : %03d\n",function_score());
     // draw a road
-    init_road(renderer);
+    init_road(renderer, background);
+    // free texture memory space
+    SDL_DestroyTexture(background);
     // free renderer memory space
     SDL_DestroyRenderer(renderer);
     // free window memory space
     SDL_DestroyWindow(window);
+    // quit SDL Image
+    //IMG_Quit();
     // quit SDL
     SDL_Quit();
 
