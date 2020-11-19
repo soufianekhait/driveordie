e21a4d8a75764f8033684792f346adb37d2e831f
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2020-11-19 22:53:37 +0100

    Define main module

---
+++ src/modules/main.h
@@ -0,0 +1,20 @@
+//
+// Created by soufiane on 17/11/2020.
+//
+
+#ifndef DRIVEORDIE_MAIN_H
+#define DRIVEORDIE_MAIN_H
+
+// include libraries
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+// define constants
+#define SCREEN_WIDTH 840
+#define SCREEN_HEIGHT 680
+
+#endif //DRIVEORDIE_MAIN_H
