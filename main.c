df77339a9c39728783a66bd7eea15eeeafabe2aa
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2020-11-19 22:55:13 +0100

    Split main into functions

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
-// main.c
-#include "Road.h"
-//#include "Car.h"
+/*
+ * This is the main function where the show begins.
+ * Including main and road modules
+ */
+#include "src/modules/main.h"
+#include "src/modules/road.h"
+#include "src/modules/car.h"
+
 
 void wait(float time)
 {
@@ -9,6 +14,7 @@ void wait(float time)
 
     while(clock()<end);
 }
+
 
 int function_score(){
     int score = 0;
@@ -36,49 +42,43 @@ int function_score(){
     return score;
 }
 
-// DOT Dimensions
-const int DOT_WIDTH = 20;
-const int DOT_HEIGHT = 20;
-// DOT Velocity
-const int DOT_VEL = 10;
 
-SDL_Window *window = NULL;
+// define a color for renderer
+const SDL_Color green = { 87, 167, 115, 255 };
 SDL_Renderer *renderer = NULL;
+SDL_Window *window = NULL;
+SDL_Texture *background = NULL;
 
-int main(int argc, char *argv[]){
-    SDL_DisplayMode dm;
-    SDL_Color orange = {255, 127, 40, 255};
-    SDL_Color green = { 87, 167, 115, 255 };
 
+int init(){
     // initialize SDL
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
         fprintf(stderr, "SDL_Init Error: %s", SDL_GetError());
         return EXIT_FAILURE;
     }
-    // get actual desktop height
-    if (SDL_GetDesktopDisplayMode(0, &dm) != 0) {
-        SDL_Log("SDL_GetDesktopDisplayMode failed: %s", SDL_GetError());
-        return EXIT_FAILURE;
-    } else{
-        // create window
-        window = SDL_CreateWindow("Drive or Die",
-                                  SDL_WINDOWPOS_CENTERED,
-                                  SDL_WINDOWPOS_CENTERED,
-                                  SCREEN_WIDTH,
-                                  //dm.h-80,
-                                  SCREEN_HEIGHT,
-                                  SDL_WINDOW_SHOWN);
-        // check if window exists
-        if(window == NULL){
-            fprintf(stderr, "SDL_CreateWindow Error: %s", SDL_GetError());
-            return EXIT_FAILURE;
-        }
-    }
+
     // initialize SDL Image
     if((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG){
         fprintf(stderr, "IMG_Init Error: %s", IMG_GetError());
         return EXIT_FAILURE;
     }
+}
+
+
+int create_renderer(){
+    // create window
+    window = SDL_CreateWindow("Drive or Die",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              SCREEN_WIDTH,
+                              SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+    // check if window exists
+    if(window == NULL){
+        fprintf(stderr, "SDL_CreateWindow Error: %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
     // create a render engine
     renderer = SDL_CreateRenderer(window, -1,SDL_RENDERER_ACCELERATED);
     // check if renderer exists
@@ -87,30 +87,20 @@ int main(int argc, char *argv[]){
         return EXIT_FAILURE;
     }
 
-    SDL_Texture *background = SDL_CreateTexture(renderer,
-                                                SDL_PIXELFORMAT_RGBA8888,
-                                                SDL_TEXTUREACCESS_TARGET,
-                                                SCREEN_WIDTH, SCREEN_HEIGHT*2);
-
-    if(background == NULL){
-        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
-        return EXIT_FAILURE;
-    }
-    //
-    // define a color for renderer
     if(SDL_SetRenderDrawColor(renderer, green.r, green.g, green.b, green.a) < 0){
         fprintf(stderr, "SDL_SetRenderDrawColor Error: %s", SDL_GetError());
         return EXIT_FAILURE;
     }
+
     // fill background with the defined color
     if(SDL_RenderClear(renderer) < 0){
         fprintf(stderr, "SDL_RenderClear Error: %s", SDL_GetError());
         return EXIT_FAILURE;
     }
-    //printf("Your final score is : %03d\n",function_score());
-    // draw a road
-    init_road(renderer, background);
-    //addCar(renderer);
+}
+
+
+void destroy(){
     // free texture memory space
     SDL_DestroyTexture(background);
     // free renderer memory space
@@ -121,6 +111,48 @@ int main(int argc, char *argv[]){
     IMG_Quit();
     // quit SDL
     SDL_Quit();
+}
+
+
+int create_texture(){
+    background = SDL_CreateTexture(renderer,
+                                   SDL_PIXELFORMAT_RGBA8888,
+                                   SDL_TEXTUREACCESS_TARGET,
+                                   SCREEN_WIDTH, SCREEN_HEIGHT*2);
+    if(background == NULL) {
+        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+}
+
+
+void draw(){
+    // draw the road
+    draw_road();
+    // draw the player
+    SDL_Surface *car_img = draw_car();
+
+    SDL_Texture *carTxt = SDL_CreateTextureFromSurface(renderer, car_img);
+    SDL_Rect car = { SCREEN_WIDTH/2 + 20, SCREEN_HEIGHT - car_img->h/3 - 10, car_img->w/3,car_img->h/3 };
+    SDL_Rect src = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+
+    move_road(carTxt, car, src);
+    destroy_car(carTxt, car_img);
+}
+
+
+int main(int argc, char *argv[]){
+    // initialize SDL
+    init();
+    // create features
+    create_renderer();
+    // create texture
+    create_texture();
+    // draw on screen
+    draw();
+    // free memory space
+    destroy();
 
     return 0;
 }
+
