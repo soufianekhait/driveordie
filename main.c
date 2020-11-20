0df3ec2343ff66d78db23fb03fbb8c7891c01401
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2020-11-20 19:16:02 +0100

    Update main

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
  * This is the main function where the show begins.
- * Including main and road modules
+ * Including modules
  */
 #include "src/modules/main.h"
 #include "src/modules/road.h"
@@ -8,47 +8,34 @@
 #include "src/modules/obstacles.h"
 
 
-void wait(float time)
-{
-    clock_t end = clock()+(time*CLOCKS_PER_SEC);
-    // Clock() retourne le nombre de tick par seconde. On calcule le moment où le wait devra s'arrêter
-
-    while(clock()<end);
-}
-
-
-int function_score(){
-    int score = 0;
-    do{
-        printf("Score = %04d\n", score);
-        wait(1); // score is incremented every sec
-        score++;
-    }while(score<=120); //While score is < 120, score++
-    do{
-        printf("Score = %04d\n", score);
-        wait(1);
-        score+=2;
-    }while(score<=240);
-    do{
-        printf("Score = %04d\n", score);
-        wait(1);
-        score+=5;
-    }while(score<=360);
-    do{
-        printf("Score = %04d\n", score);
-        wait(1);
-        score+=10;
-    }while(score>=360);
-
-    return score;
-}
-
-
 // define a color for renderer
 const SDL_Color green = { 87, 167, 115, 255 };
 SDL_Renderer *renderer = NULL;
 SDL_Window *window = NULL;
 SDL_Texture *background = NULL;
+
+// signatures
+int init();
+int create_renderer();
+int create_texture();
+void draw();
+void destroy();
+
+
+int main(int argc, char *argv[]){
+    // initialize SDL
+    init();
+    // create features
+    create_renderer();
+    // create texture
+    create_texture();
+    // draw on screen
+    draw();
+    // free memory space
+    destroy();
+
+    return 0;
+}
 
 
 int init(){
@@ -101,6 +88,33 @@ int create_renderer(){
 }
 
 
+int create_texture(){
+    background = SDL_CreateTexture(renderer,
+                                   SDL_PIXELFORMAT_RGBA8888,
+                                   SDL_TEXTUREACCESS_TARGET,
+                                   SCREEN_WIDTH, SCREEN_HEIGHT*2);
+    if(background == NULL) {
+        fprintf(stderr, "SDL_CreateTexture Error: %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+}
+
+
+void draw(){
+    // draw the road
+    draw_road();
+    // draw the player
+    load_car();
+    // draw the obstacle
+    load_obstacle();
+    // move road
+    move_road();
+
+    destroy_car(car_txt, car_img);
+    destroy_obstacle(obstacle_img);
+}
+
+
 void destroy(){
     // free texture memory space
     SDL_DestroyTexture(background);
@@ -113,53 +127,3 @@ void destroy(){
     // quit SDL
     SDL_Quit();
 }
-
-
-int create_texture(){
-    background = SDL_CreateTexture(renderer,
-                                   SDL_PIXELFORMAT_RGBA8888,
-                                   SDL_TEXTUREACCESS_TARGET,
-                                   SCREEN_WIDTH, SCREEN_HEIGHT*2);
-    if(background == NULL) {
-        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
-        return EXIT_FAILURE;
-    }
-}
-
-
-void draw(){
-    // draw the road
-    draw_road();
-    // draw the player
-    SDL_Surface *car_img = draw_car();
-    SDL_Surface *obsctale_img = draw_obstacles();
-
-    SDL_Texture *carTxt = SDL_CreateTextureFromSurface(renderer, car_img);
-    SDL_Rect car = { SCREEN_WIDTH/2 + 20, SCREEN_HEIGHT - car_img->h/3 - 10, car_img->w/3,car_img->h/3 };
-    SDL_Rect src = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
-
-    SDL_Texture *texture_obstacle = SDL_CreateTextureFromSurface(renderer, obsctale_img);
-    SDL_Texture *texture_obstacle2 = SDL_CreateTextureFromSurface(renderer, obsctale_img);
-    SDL_Texture *texture_obstacle3 = SDL_CreateTextureFromSurface(renderer, obsctale_img);
-
-    move_road(carTxt, car, src, texture_obstacle, texture_obstacle2, texture_obstacle3, obsctale_img);
-    destroy_car(carTxt, car_img);
-    destroy_obstacles(obsctale_img);
-}
-
-
-int main(int argc, char *argv[]){
-    // initialize SDL
-    init();
-    // create features
-    create_renderer();
-    // create texture
-    create_texture();
-    // draw on screen
-    draw();
-    // free memory space
-    destroy();
-
-    return 0;
-}
-
